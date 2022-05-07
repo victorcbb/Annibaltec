808b3644f450091fed70ceeb9071ac3759a22e0f
Victor Barros <victorclovisb@hotmail.com>
2022-05-07 08:50:34 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+window.addEventListener("scroll", onScroll)
+
+function onScroll() {
+    showNavOnScroll()
+    showBackToTopButton()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+    
+    const sectionEndAt = sectionTop + sectionHeight
+    const sectionEndPassedTargetLine = sectionEndAt <= targetLine
+
+    const sectionBaundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+
+    if (sectionBaundaries) {
+        menuElement.classList.add('active')
+        console.log(menuElement)
+    }
+
+}
+
+function showNavOnScroll() {
+    if (scrollY > 0) {
+        navigation.classList.add("scroll")
+    } else {
+        navigation.classList.remove("scroll")
+    }
+}
+
+function showBackToTopButton() {
+    if (scrollY > 500) {
+        backToTopButton.classList.add("show")
+    } else {
+        backToTopButton.classList.remove("show")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add("menu-expanded")
+}
+
+function closeMenu() {
+    document.body.classList.remove("menu-expanded")
+}
+
+ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 700
+}).reveal(`
+    #home, 
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about .content
+`);
